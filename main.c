42f82e9519693c93121e3edf664b8f47ef1fb96d
spacecoffin <reed.rosenberg@gmail.com>
2014-12-12 14:22:51 -0500

    Initial commit
    Hello world

---
+++ main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  CS 211 - Fall 14
+//  Reed Rosenberg
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
