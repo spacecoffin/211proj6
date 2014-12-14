c99765a3da280da11b00ca6ee57c4c60879cfa5d
spacecoffin <reed.rosenberg@gmail.com>
2014-12-14 01:13:19 -0500

    added GNU example
    cool formatting

--- main.c
+++ main.c
@@ -5,12 +5,20 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>     // as suggested in 17.8 - Pseudo Terminals
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+        // 1. Print the command prompt.
+        printf("myshell-> ");
+        
+        // 2. Read the command line (e.g, “ls -a -l”).
+        
+        
+        // 3. Parse the command line into tokens consisting of the executable
+        //      file name (“ls”) and its arguments (“-a” and “-l”).
+        // 4. Fork a child process, which executes the command.
+        // 5. Wait for the child to terminate.
+        return 0;
 }
 
